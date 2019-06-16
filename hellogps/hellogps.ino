08f17edcef9f96e115083964aeba3352c56546af
patrick <paddel10@hotmail.com>
2019-06-16 14:53:17 +0200

    cleanup

--- hellogps/hellogps.ino
+++ hellogps/hellogps.ino
@@ -1,115 +1,46 @@
 #include <SoftwareSerial.h>
-#include <HardwareSerial.h>
-SoftwareSerial GPSModule(10, 11); // RX, TX
-int updates;
-int failedUpdates;
-int pos;
-int stringplace = 0;
 
-String timeUp;
-String nmea[15];
-String labels[12] {"Time: ", "Status: ", "Latitude: ", "Hemisphere: ", "Longitude: ", "Hemisphere: ", "Speed: ", "Track Angle: ", "Date: "};
-void setup() {
-  Serial.begin(57600);
-  Serial.println("setup() called");
-  GPSModule.begin(9600);
+
+#define LED_RED_PIN 13
+
+#define RX_PIN 10 // connect to rx pin in GPS connection
+#define TX_PIN 11 // connect to tx pin in GPS connection
+
+#define GPRMC_STR "$GPRMC,"
+
+SoftwareSerial GPSModule(RX_PIN, TX_PIN);
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_RED_PIN, OUTPUT);
+    Serial.begin(57600);
+    Serial.println("setup() called");
+    GPSModule.begin(9600);
 }
 
-void loop() {
-  Serial.flush();
-  Serial.println("loop() called");
-  GPSModule.flush();
-  while (GPSModule.available() > 0)
-  {
-    GPSModule.read();
-  }
-  if (GPSModule.find("$GPRMC,")) {
-    String tempMsg = GPSModule.readStringUntil('\n');
-    for (int i = 0; i < tempMsg.length(); i++) {
-      if (tempMsg.substring(i, i + 1) == ",") {
-        nmea[pos] = tempMsg.substring(stringplace, i);
-        stringplace = i + 1;
-        pos++;
-      }
-      if (i == tempMsg.length() - 1) {
-        nmea[pos] = tempMsg.substring(stringplace, i);
-      }
+void loop()
+{
+    Serial.flush();
+    GPSModule.flush();
+    while (GPSModule.available() > 0) {
+        String gpsData = GPSModule.readStringUntil('\n');
+        if (gpsData.startsWith(GPRMC_STR)) {
+            if (16 == gpsData.indexOf(",A,")) {
+                // valid data
+                Serial.print(gpsData);
+                digitalWrite(LED_BUILTIN, HIGH);
+                digitalWrite(LED_RED_PIN, HIGH);
+                continue;
+            }
+            if (16 == gpsData.indexOf(",V,")) {
+                // invalid data
+                Serial.println("void");
+            }
+            digitalWrite(LED_BUILTIN, LOW);
+            digitalWrite(LED_RED_PIN, LOW);
+        }
     }
-    updates++;
-    nmea[2] = ConvertLat();
-    nmea[4] = ConvertLng();
-    for (int i = 0; i < 9; i++) {
-      Serial.print(labels[i]);
-      Serial.print(nmea[i]);
-      Serial.println("");
-    }
-
-  }
-  else {
-
-    failedUpdates++;
-
-  }
-  stringplace = 0;
-  pos = 0;
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_RED_PIN, LOW);
 }
-
-String ConvertLat() {
-  String posneg = "";
-  if (nmea[3] == "S") {
-    posneg = "-";
-  }
-  String latfirst;
-  float latsecond;
-  for (int i = 0; i < nmea[2].length(); i++) {
-    if (nmea[2].substring(i, i + 1) == ".") {
-      latfirst = nmea[2].substring(0, i - 2);
-      latsecond = nmea[2].substring(i - 2).toFloat();
-    }
-  }
-  latsecond = latsecond / 60;
-  String CalcLat = "";
-
-  char charVal[9];
-  dtostrf(latsecond, 4, 6, charVal);
-  for (int i = 0; i < sizeof(charVal); i++)
-  {
-    CalcLat += charVal[i];
-  }
-  latfirst += CalcLat.substring(1);
-  latfirst = posneg += latfirst;
-  return latfirst;
-}
-
-String ConvertLng() {
-  String posneg = "";
-  if (nmea[5] == "W") {
-    posneg = "-";
-  }
-
-  String lngfirst;
-  float lngsecond;
-  for (int i = 0; i < nmea[4].length(); i++) {
-    if (nmea[4].substring(i, i + 1) == ".") {
-      lngfirst = nmea[4].substring(0, i - 2);
-      //Serial.println(lngfirst);
-      lngsecond = nmea[4].substring(i - 2).toFloat();
-      //Serial.println(lngsecond);
-
-    }
-  }
-  lngsecond = lngsecond / 60;
-  String CalcLng = "";
-  char charVal[9];
-  dtostrf(lngsecond, 4, 6, charVal);
-  for (int i = 0; i < sizeof(charVal); i++)
-  {
-    CalcLng += charVal[i];
-  }
-  lngfirst += CalcLng.substring(1);
-  lngfirst = posneg += lngfirst;
-  return lngfirst;
-}
-
-
-
